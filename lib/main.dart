4877ee7ee2c88b3a45b138f222dff7efae4f86f0
kennan_robin <63162744+132kibsdanger@users.noreply.github.com>
2022-02-16 10:07:24 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors, avoid_init_to_null, unused_local_variable, avoid_print
 
-import 'package:flutter/material.dart';
+impor 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
