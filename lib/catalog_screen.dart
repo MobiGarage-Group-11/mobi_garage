bad4fd90dcc11cc277de0de4eb3159dee15dc212
kennan_robin <63162744+132kibsdanger@users.noreply.github.com>
2022-02-16 09:59:07 +0300

    Update catalog_screen.dart

--- lib/catalog_screen.dart
+++ lib/catalog_screen.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:flutter/material.dart';
+impor 'package:flutter/material.dart';
 
 import 'cart_screens.dart';
 import 'catalog_products.dart';
