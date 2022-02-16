aa108933e68e91e641be4125869c6dcb82c00787
kennan_robin <63162744+132kibsdanger@users.noreply.github.com>
2022-02-16 10:03:52 +0300

    Update login_screen.dart

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-impor 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
 import 'package:mobi_garage/user.dart';
